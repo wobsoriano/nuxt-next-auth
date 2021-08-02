bcb76ffc7993baf8a604cd3c1c717ce951db3845
Robert Soriano <sorianorobertc@gmail.com>
2021-08-02 23:18:44 +0800

    publish v0.0.1

---
+++ test/fixture/nuxt.config.js
@@ -0,0 +1,18 @@
+import Providers from 'next-auth/providers';
+import NuxtNextAuth from '../../module';
+
+export default {
+  buildModules: ['@nuxtjs/composition-api/module', '@nuxt/typescript-build'],
+  modules: [NuxtNextAuth],
+  vite: {
+    ssr: true
+  },
+  nextAuth: {
+    providers: [
+      Providers.GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+      })
+    ]
+  }
+};
