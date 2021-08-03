7a9233d416727ed075d7efc37f6b6e9a270e6086
Robert Soriano <sorianorobertc@gmail.com>
2021-08-03 12:17:43 +0800

    add composition api page example

---
+++ test/fixture/pages/composition.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-if="session">
+            Signed in as {{ session.user.email }} <br />
+            <button @click="signOut">Sign out</button>
+        </div>
+        <div v-else>
+            Not signed in <br/>
+            <button @click="signIn">Sign in</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import { signIn, signOut, useSession } from '../../../src/client'
+
+export default defineComponent({
+    setup() {
+        const [session, loading] = useSession()
+
+        return {
+            session,
+            loading,
+            signIn,
+            signOut
+        }
+    }
+})
+</script>
