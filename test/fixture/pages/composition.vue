676f12efb467acc6205dbb70a77c78f499da2a4b
Robert Soriano <sorianorobertc@gmail.com>
2021-08-03 13:01:49 +0800

    lint fix

--- test/fixture/pages/composition.vue
+++ test/fixture/pages/composition.vue
@@ -1,30 +1,30 @@
 <template>
-    <div>
-        <div v-if="session">
-            Signed in as {{ session.user.email }} <br />
-            <button @click="signOut">Sign out</button>
-        </div>
-        <div v-else>
-            Not signed in <br/>
-            <button @click="signIn">Sign in</button>
-        </div>
+  <div>
+    <div v-if="session">
+      Signed in as {{ session.user.email }} <br />
+      <button @click="signOut">Sign out</button>
     </div>
+    <div v-else>
+      Not signed in <br />
+      <button @click="signIn">Sign in</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
-import { signIn, signOut, useSession } from '../../../src/client'
+import { defineComponent } from '@nuxtjs/composition-api';
+import { signIn, signOut, useSession } from '../../../src/client';
 
 export default defineComponent({
-    setup() {
-        const [session, loading] = useSession()
+  setup() {
+    const [session, loading] = useSession();
 
-        return {
-            session,
-            loading,
-            signIn,
-            signOut
-        }
-    }
-})
+    return {
+      session,
+      loading,
+      signIn,
+      signOut
+    };
+  }
+});
 </script>
